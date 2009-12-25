72ed8f3feb7313e17dd188f1c742f893e5b0838b
mdew <mdew@mdew-desktop>
2009-12-25 16:26:34 +1300

    fix script

--- script.sh
+++ script.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 perl addChecksum.pl fanboy-adblocklist-adult.txt
 perl addChecksum.pl fanboy-adblocklist-current-expanded.txt
-perl addChecksum.pl fanboy-adblocklist-cz.txt
 perl addChecksum.pl fanboy-adblocklist-stats.txt  
 perl addChecksum.pl firefox-regional/fanboy-adblocklist-cz.txt
 perl addChecksum.pl firefox-regional/fanboy-adblocklist-jpn.txt
 perl addChecksum.pl firefox-regional/fanboy-adblocklist-esp.txt
 perl addChecksum.pl firefox-regional/fanboy-adblocklist-tky.txt
-hl add .
+hg add .
 hg commit -m "$1"
 hg push
