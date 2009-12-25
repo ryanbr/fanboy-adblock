03f0a9080f8cfb554b5a5b8d835fa4ba4396b50a
mdew <mdew@mdew-desktop>
2009-12-25 16:21:15 +1300

    move to proper place

--- script.sh
+++ script.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
-hg add .
+perl addChecksum.pl fanboy-adblocklist-adult.txt
+perl addChecksum.pl fanboy-adblocklist-current-expanded.txt
+perl addChecksum.pl fanboy-adblocklist-cz.txt
+perl addChecksum.pl fanboy-adblocklist-stats.txt  
+perl addChecksum.pl firefox-regional/fanboy-adblocklist-cz.txt
+perl addChecksum.pl firefox-regional/fanboy-adblocklist-jpn.txt
+perl addChecksum.pl firefox-regional/fanboy-adblocklist-esp.txt
+perl addChecksum.pl firefox-regional/fanboy-adblocklist-tky.txt
+hl add .
 hg commit -m "$1"
 hg push
