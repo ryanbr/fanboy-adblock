196caab88ffef03e1a6dd9f0404563bdb4afd51f
mdew <mdew@mdew-desktop>
2010-01-01 00:46:34 +1300

    prepare for 023A release

--- script.sh
+++ script.sh
@@ -3,15 +3,16 @@
 perl addChecksum.pl fanboy-adblocklist-adult.txt
 perl addChecksum.pl fanboy-adblocklist-current-expanded.txt
 perl addChecksum.pl fanboy-adblocklist-stats.txt  
+perl addChecksum.pl fanboy-adblocklist-dimensions-v2.txt
+# Firefox Regional lists
 perl addChecksum.pl firefox-regional/fanboy-adblocklist-cz.txt
+perl addChecksum.pl firefox-regional/fanboy-adblocklist-chn.txt
+perl addChecksum.pl firefox-regional/fanboy-adblocklist-krn.txt
 perl addChecksum.pl firefox-regional/fanboy-adblocklist-jpn.txt
 perl addChecksum.pl firefox-regional/fanboy-adblocklist-esp.txt
 perl addChecksum.pl firefox-regional/fanboy-adblocklist-tky.txt
 # Opera
 perl addChecksum-opera.pl opera/urlfilter.ini
-perl addChecksum-opera.pl opera/urlfilter-tky.ini
-perl addChecksum-opera.pl opera/urlfilter-esp.ini
-perl addChecksum-opera.pl opera/urlfilter-jpn.ini
 perl addChecksum.pl opera/fanboy-adblocklist-elements-v3.css
 # Iron
 perl addChecksum-opera.pl iron/adblock-beta.ini
