c44e70bf0d0ec6f383c509e1efc32dff7df81782
Hubird.au <devnull@localhost>
2010-07-30 20:17:48 +1000

    script.sh add new regional

--- script.sh
+++ script.sh
@@ -22,6 +22,7 @@ perl addChecksum.pl firefox-regional/fanboy-adblocklist-swe.txt
 perl addChecksum.pl firefox-regional/fanboy-adblocklist-tky.txt
 perl addChecksum.pl firefox-regional/fanboy-adblocklist-ita.txt
 perl addChecksum.pl firefox-regional/fanboy-adblocklist-vtn.txt
+perl addChecksum.pl firefox-regional/fanboy-adblocklist-tam.txt
 # Opera
 perl addChecksum-opera.pl opera/urlfilter.ini
 perl addChecksum.pl opera/fanboy-adblocklist-elements-v3.css
