c8599a88283532aa11e22d38d8d4975b3644eaec
mp3geek <mp3geek@gmail.com>
2011-04-17 15:18:42 +1200

    fix spacing

--- scripts/ie/adblock-ie-generator.sh
+++ scripts/ie/adblock-ie-generator.sh
@@ -9,7 +9,7 @@
 # Creating a 10Mb ramdisk Temp storage...
 #
 if [ ! -d "/tmp/ieramdisk/" ]; then
-		rm -rf /tmp/ieramdisk/
+    rm -rf /tmp/ieramdisk/
     mkdir /tmp/ieramdisk; chmod 777 /tmp/ieramdisk
     mount -t tmpfs -o size=10M tmpfs /tmp/ieramdisk/
     mkdir /tmp/ieramdisk/subscriptions
