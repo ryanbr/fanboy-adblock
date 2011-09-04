a18ed4fb80503b7b54cd7d5dd064f7c39dea77d2
mp3geek <mp3geek@gmail.com>
2011-09-04 14:49:32 +1200

    rebuild opera filters

--- docs/Sorting/sorter.pl
+++ docs/Sorting/sorter.pl
@@ -39,7 +39,7 @@ my $current = [];
 # Check "!", "[]" and "#" and ";" (Firefox and Opera)
 #
 while ( <$fh> ) {
-    if ( m/^[!#;]|^\[]/ ) {
+    if ( m/^(?:[!\[]|[#|;]\s)/ ) {
         output $current, $fhout;
         $current = [ $_ ];
     }
