524288075570bf433db4fcd48dc636332f8a1400
mp3geek <mp3geek@gmail.com>
2012-01-23 17:20:57 +1300

    simiplify sorting script by allowing multiple text files at the same time

--- add-filter.bat
+++ add-filter.bat
@@ -8,40 +8,13 @@ hg pull -u
 :: b
 hg b add -e "%*"
 :: pre-sort
-perl docs\sorting\sorter.pl fanboy-adblocklist-current-expanded.txt
-perl docs\sorting\sorter.pl fanboy-adblocklist-stats.txt
-perl docs\sorting\sorter.pl fanboy-adblocklist-addon.txt
-perl docs\sorting\sorter.pl other\adblock-gannett.txt
-perl docs\sorting\sorter.pl other\enhancedstats-addon.txt
+perl docs\sorting\sorter2.pl fanboy-adblocklist-current-expanded.txt fanboy-adblocklist-stats.txt fanboy-adblocklist-addon.txt other\adblock-gannett.txt other\enhancedstats-addon.txt
 :: Firefox Regional lists
-perl docs\sorting\sorter.pl firefox-regional\fanboy-adblocklist-cz.txt
-perl docs\sorting\sorter.pl firefox-regional\fanboy-adblocklist-esp.txt
-perl docs\sorting\sorter.pl firefox-regional\fanboy-adblocklist-ind.txt
-perl docs\sorting\sorter.pl firefox-regional\fanboy-adblocklist-ita.txt
-perl docs\sorting\sorter.pl firefox-regional\fanboy-adblocklist-jpn.txt
-perl docs\sorting\sorter.pl firefox-regional\fanboy-adblocklist-krn.txt
-perl docs\sorting\sorter.pl firefox-regional\fanboy-adblocklist-pol.txt
-perl docs\sorting\sorter.pl firefox-regional\fanboy-adblocklist-rus-v2.txt
-perl docs\sorting\sorter.pl firefox-regional\fanboy-adblocklist-swe.txt
-perl docs\sorting\sorter.pl firefox-regional\fanboy-adblocklist-tky.txt
-perl docs\sorting\sorter.pl firefox-regional\fanboy-adblocklist-vtn.txt
+perl docs\sorting\sorter2.pl firefox-regional\fanboy-adblocklist-cz.txt firefox-regional\fanboy-adblocklist-esp.txt firefox-regional\fanboy-adblocklist-ind.txt firefox-regional\fanboy-adblocklist-ita.txt firefox-regional\fanboy-adblocklist-jpn.txt firefox-regional\fanboy-adblocklist-krn.txt firefox-regional\fanboy-adblocklist-pol.txt firefox-regional\fanboy-adblocklist-rus-v2.txt firefox-regional\fanboy-adblocklist-swe.txt firefox-regional\fanboy-adblocklist-tky.txt firefox-regional\fanboy-adblocklist-vtn.txt
 :: Opera
-perl docs\sorting\sorter.pl opera\urlfilter.ini
-perl docs\sorting\sorter.pl opera\urlfilter-cz.ini
-perl docs\sorting\sorter.pl opera\urlfilter-esp.ini
-perl docs\sorting\sorter.pl opera\urlfilter-ind.ini
-perl docs\sorting\sorter.pl opera\urlfilter-jpn.ini
-perl docs\sorting\sorter.pl opera\urlfilter-krn.ini
-perl docs\sorting\sorter.pl opera\urlfilter-pol.ini
-perl docs\sorting\sorter.pl opera\urlfilter-rus.ini
-perl docs\sorting\sorter.pl opera\urlfilter-stats.ini
-perl docs\sorting\sorter.pl opera\urlfilter-tky.ini
-perl docs\sorting\sorter.pl opera\urlfilter-vtn.ini
-perl docs\sorting\sorter.pl opera\urlfilter-swe.ini
+perl docs\sorting\sorter2.pl opera\urlfilter.ini opera\urlfilter-cz.ini opera\urlfilter-esp.ini opera\urlfilter-ind.ini opera\urlfilter-jpn.ini opera\urlfilter-krn.ini opera\urlfilter-pol.ini opera\urlfilter-rus.ini opera\urlfilter-stats.ini opera\urlfilter-tky.ini opera\urlfilter-vtn.ini opera\urlfilter-swe.ini
 :: IE Addon List
-perl docs\sorting\sorter.pl ie\fanboy-adblock-addon.txt
-perl docs\sorting\sorter.pl ie\fanboy-tracking-addon.txt
-perl docs\sorting\sorter.pl ie\fanboy-russian-addon.txt
+perl docs\sorting\sorter2.pl ie\fanboy-adblock-addon.txt ie\fanboy-tracking-addon.txt ie\fanboy-russian-addon.txt
 :: Firefox
 perl scripts\addChecksum.pl fanboy-adblocklist-current-expanded.txt
 perl scripts\addChecksum.pl fanboy-adblocklist-stats.txt
