211c65f107ee078c58e7300d07e60fffe2f58d57
mp3geek <mp3geek@gmail.com>
2012-03-18 18:40:47 +1300

    Add error checking to vtn-merged

--- scripts/combine/firefox-adblock-vtn.sh
+++ scripts/combine/firefox-adblock-vtn.sh
@@ -11,6 +11,7 @@ MAINDIR="/var/www/adblock"
 GOOGLEDIR="/home/fanboy/google/fanboy-adblock-list"
 TESTDIR="/tmp/ramdisk"
 ZIP="/usr/local/bin/7za"
+NICE="nice -n 19"
 
 # Make Ramdisk.
 #
@@ -41,13 +42,50 @@ sed '$d' < $TESTDIR/fanboy-vtn-temp.txt > $TESTDIR/fanboy-vtn-temp2.txt
 # Merge to the files together
 #
 cat $MAINDIR/fanboy-adblock.txt $TESTDIR/fanboy-vtn-temp2.txt > $TESTDIR/fanboy-vtn-merged.txt
+
+# Create a backup incase addchecksum "zeros" the file
+#
+cp -f $TESTDIR/fanboy-vtn-merged.txt $TESTDIR/fanboy-vtn-merged-bak.txt
+
+# Add checksum
+#
 perl $MAINDIR/addChecksum.pl $TESTDIR/fanboy-vtn-merged.txt
 
-# Copy Merged file to main dir
-#
-cp $TESTDIR/fanboy-vtn-merged.txt $MAINDIR/r/fanboy+vietnam.txt
+if [ -s $TESTDIR/fanboy-vtn-merged.txt ];
+then
+  # Copy Merged file to main dir
+  #
+  cp $TESTDIR/fanboy-vtn-merged.txt $MAINDIR/r/fanboy+vietnam.txt
 
-# Compress file
-#
-rm -f $MAINDIR/r/fanboy+vietnam.txt.gz
-$ZIP a -mx=9 -y -tgzip $MAINDIR/r/fanboy+vietnam.txt.gz $MAINDIR/r/fanboy+vietnam.txt > /dev/null
+  # Compress file
+  #
+  rm -f $MAINDIR/r/fanboy+vietnam.txt.gz
+  $NICE $ZIP a -mx=9 -y -tgzip $MAINDIR/r/fanboy+vietnam.txt.gz $TESTDIR/fanboy-vtn-merged.txt > /dev/null > /dev/null
+  
+  # log
+  ### echo "Updated fanboy+vietnam.txt"
+  echo "Updated /r/fanboy+vietnam.txt (merged fanboy+vietnam) (script: firefox-adblock-vtn.sh) on `date +'%Y-%m-%d %H:%M:%S'`" >> /var/log/adblock-log.txt
+  # Clear old Variables
+  #
+  rm -rf $TESTDIR/fanboy-vtn-*
+else
+  # Add checksum
+  #
+  sleep 2
+  perl $MAINDIR/addChecksum.pl $TESTDIR/fanboy-vtn-merged-bak.txt
+  
+  # Copy Merged file to main dir
+  #
+  cp $TESTDIR/fanboy-vtn-merged-bak.txt $MAINDIR/r/fanboy+vietnam.txt
+   
+  # Compress file
+  #
+  rm -f $MAINDIR/r/fanboy+vietnam.txt.gz
+  
+  ### echo "Updated fanboy+vietnam.txt (checksum zerod file)"
+  $NICE $ZIP a -mx=9 -y -tgzip $MAINDIR/r/fanboy+vietnam.txt.gz $TESTDIR/fanboy-vtn-merged-bak.txt > /dev/null > /dev/null
+  echo "*** ERROR ***: Addchecksum Zero'd the file: fanboy+vietnam.txt (script: firefox-adblock-vtn.sh) on `date +'%Y-%m-%d %H:%M:%S'`" >> /var/log/adblock-log.txt
+  # Clear old Variables
+  #
+  rm -rf $TESTDIR/fanboy-vtn-*
+fi
