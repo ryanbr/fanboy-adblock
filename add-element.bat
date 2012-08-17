812ade9ae0540e8b03bc76e48b880d9ff7643604
fanboy <devnull@localhost>
2012-08-18 01:58:30 +1200

    put on hold

--- add-element.bat
+++ add-element.bat
@@ -9,7 +9,7 @@ perl docs\sorting\sorter2.pl opera\urlfilter.ini opera\urlfilter-cz.ini opera\ur
 :: IE Addon List
 perl docs\sorting\sorter2.pl ie\fanboy-adblock-addon.txt ie\fanboy-tracking-addon.txt ie\fanboy-russian-addon.txt
 :: Firefox
-perl scripts\addChecksum.pl fanboy-adblocklist-current-expanded.txt fanboy-adblocklist-stats.txt fanboy-adblocklist-addon.txt other\chrome-addon.txt enhancedstats-addon.txt other\tracking-intl.txt
+perl scripts\addChecksum.pl fanboy-adblocklist-current-expanded.txt fanboy-adblocklist-stats.txt fanboy-adblocklist-addon.txt other\adblock-gannett.txt other\chrome-addon.txt enhancedstats-addon.txt other\tracking-intl.txt
 :: Firefox Regional lists
 perl scripts\addChecksum.pl firefox-regional\fanboy-adblocklist-cz.txt firefox-regional\fanboy-adblocklist-esp.txt firefox-regional\fanboy-adblocklist-ita.txt firefox-regional\fanboy-adblocklist-jpn.txt firefox-regional\fanboy-adblocklist-krn.txt firefox-regional\fanboy-adblocklist-rus-v2.txt firefox-regional\fanboy-adblocklist-swe.txt firefox-regional\fanboy-adblocklist-tky.txt firefox-regional\fanboy-adblocklist-vtn.txt firefox-regional\fanboy-adblocklist-ind.txt firefox-regional\fanboy-adblocklist-pol.txt
 :: Opera
