c23ba5c605000df905594c486d45f79654232567
fanboy <devnull@localhost>
2012-08-29 20:20:14 +1200

    re-write fanboy-ie script generator

--- scripts/ie/adblock-ie-generator.sh
+++ scripts/ie/adblock-ie-generator.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 #
-# Fanboy Adblock IE Convert script v1.4 (25/08/2012)
+# Fanboy Adblock IE Convert script v1.5 (29/08/2012)
 # Dual License CCby3.0/GPLv2
 # http://creativecommons.org/licenses/by/3.0/
 # http://www.gnu.org/licenses/gpl-2.0.html
 #
 # Variables
 #
+# Version history
+#
+# 1.50 Re-write script to be cleaner and more readable, better error checking.
 
 export ZIP="nice -n 19 /usr/local/bin/7za a -mx=9 -y -tgzip"
 export NICE="nice -n 19"
@@ -24,15 +27,15 @@ export IEDIR="/tmp/work/ie"
 export IESUBS="/tmp/work/ie/subscriptions"
 export IRONDIR="/tmp/Ramdisk/www/adblock/iron"
 
-
 # Check for temp ie stuff
 #
 if [ ! -d "$IEDIR" ]; then
     rm -rf $IEDIR
     mkdir $IEDIR; chmod 777 $IEDIR
+    mkdir $IESUBS; chmod 777 $IESUBS
 fi
 
-if [ ! -d "$IEDIR/combineSubscriptions.py" ]; then
+if [ ! -s "$IEDIR/combineSubscriptions.py" ]; then
     cp -f $HGSERV/scripts/ie/combineSubscriptions.py $IEDIR
 fi
 
@@ -47,57 +50,57 @@ if [ -d "$IEDIR" ]; then
     rm -rf $IEDIR/*.txt
 fi
 
-# Cleanup fanboy-adblock-addon.txt (remove the top 8 lines)
+# Check we have a generated non-element file before processing.
 #
-sed '1,8d' $HGSERV/ie/fanboy-adblock-addon.txt > $IEDIR/fanboy-adblock-addon.txt
+if [ -s "$TESTDIR/fanboy-non-element.txt" ] then
+      # Cleanup fanboy-adblock-addon.txt (remove the top 8 lines)
+      #
+      sed '1,8d' $HGSERV/ie/fanboy-adblock-addon.txt > $TESTDIR/fanboy-adblock-ie-addon.txt
 
-# Merge with Google-code (IE adblock addon)
-#
+      # Standard Adblock Filter
+      cat $TESTDIR/fanboy-non-element.txt \
+          $TESTDIR/fanboy-adblock-ie-addon.txt > $IEDIR/fanboy-noele.txt
+      # IE Ultimate
+      cat $TESTDIR/fanboy-non-element.txt \
+          $TESTDIR/fanboy-adblock-ie-addon.txt \
+          $MAINDIR/fanboy-tracking.txt \
+          $MAINDIR/fanboy-addon.txt \
+          $MAINDIR/enhancedstats.txt > $IEDIR/fanboy-ultimate-ie.txt
+      # IE Complete
+      cat $TESTDIR/fanboy-non-element.txt \
+          $TESTDIR/fanboy-adblock-ie-addon.txt \
+          $MAINDIR/fanboy-tracking.txt \
+          $MAINDIR/enhancedstats.txt > $IEDIR/fanboy-complete-ie.txt
 
-cat $HGSERV/fanboy-adblock/fanboy-header.txt $HGSERV/fanboy-adblock/fanboy-generic.txt $HGSERV/fanboy-adblock/fanboy-thirdparty.txt $HGSERV/fanboy-adblock/fanboy-firstparty.txt \
-        $HGSERV/fanboy-adblock/fanboy-whitelist.txt $HGSERV/fanboy-adblock/fanboy-dimensions.txt \
-        $HGSERV/fanboy-adblock/fanboy-dimensions-whitelist.txt $HGSERV/fanboy-adblock/fanboy-adult-generic.txt $HGSERV/fanboy-adblock/fanboy-adult-firstparty.txt \
-        $HGSERV/fanboy-adblock/fanboy-adult-thirdparty.txt $HGSERV/fanboy-adblock/fanboy-adult-whitelists.txt \
-        $HGSERV/fanboy-adblock/fanboy-p2p-firstparty.txt $HGSERV/fanboy-adblock/fanboy-p2p-thirdparty.txt > $IEDIR/fanboy-adblock-noele.txt
+      # Remove ~third-party
+      #
+      sed -i '/~third-party/d' $IEDIR/fanboy-noele.txt $IEDIR/fanboy-ultimate-ie.txt $IEDIR/fanboy-complete-ie.txt
 
-cat $MAINDIR/fanboy-adblock-noele.txt $IEDIR/fanboy-adblock-addon.txt > $IEDIR/fanboy-noele.txt
+      # Generate .tpl IE list
+      #
+      python $IEDIR/combineSubscriptions.py $IEDIR $IESUBS
 
-# IE Ultimate and Complete
-#
-cat $IEDIR/fanboy-noele.txt $MAINDIR/fanboy-ultimate-ie.txt > $IEDIR/fanboy-ultimate-ie.txt
-cat $IEDIR/fanboy-noele.txt $MAINDIR/fanboy-complete-ie.txt > $IEDIR/fanboy-complete-ie.txt
+      # Remove Generated gzip filters, we dont need these yet
+      #
+      rm -f $IESUBS/fanboy-noele.tpl*.gz
+      rm -f $IESUBS/fanboy-ultimate-*.gz
+      rm -f $IESUBS/fanboy-complete-*.gz
 
-# Remove Old files
-#
-rm -rf $IEDIR/fanboy-adblock-noele.txt $IEDIR/fanboy-adblock-addon.txt
+      # Cleanup Script (removed filters not compatible with IE)
+      #
+      $HGSERV/scripts/ie/ie-cleanup-filters.sh
 
-# Remove ~third-party
-#
-sed -i '/~third-party/d' $IEDIR/fanboy-noele.txt $IEDIR/fanboy-ultimate-ie.txt $IEDIR/fanboy-complete-ie.txt
+      # Re-compress newly modified file
+      #
+      $ZIP $IESUBS/fanboy-noele.tpl.gz $IESUBS/fanboy-noele.tpl &> /dev/null
+      $ZIP $IESUBS/fanboy-ultimate-ie.tpl.gz $IESUBS/fanboy-ultimate-ie.tpl &> /dev/null
+      $ZIP $IESUBS/fanboy-complete-ie.tpl.gz $IESUBS/fanboy-complete-ie.tpl &> /dev/null
 
-# Generate .tpl IE list
-#
-python $IEDIR/combineSubscriptions.py $IEDIR $SUBS
+      # Now copy finished tpl list to the website.
+      #
+      cp -f $IESUBS/fanboy-noele.tpl* $IESUBS/fanboy-ultimate-ie.tpl* $IESUBS/fanboy-complete-ie.tpl* $MAINDIR/ie/
 
-# Cleanup Script
-#
-$HGSERV/scripts/ie/ie-cleanup-filters.sh
-
-# Remove old gz file
-#
-rm -f $SUBS/fanboy-noele.tpl*.gz
-rm -f $SUBS/fanboy-ultimate-*.gz
-rm -f $SUBS/fanboy-complete-*.gz
-
-# Re-compress newly modified file
-#
-$ZIP $SUBS/fanboy-noele.tpl.gz $SUBS/fanboy-noele.tpl > /dev/null
-$ZIP $SUBS/fanboy-ultimate-ie.tpl.gz $SUBS/fanboy-ultimate-ie.tpl > /dev/null
-$ZIP $SUBS/fanboy-complete-ie.tpl.gz $SUBS/fanboy-complete-ie.tpl > /dev/null
-
-# Now copy finished tpl list to the website.
-#
-cp -f $SUBS/fanboy-noele.tpl* $MAINDIR/ie/
-cp -f $SUBS/fanboy-ultimate-ie.tpl* $MAINDIR/ie/
-cp -f $SUBS/fanboy-complete-ie.tpl* $MAINDIR/ie/
-
+      # Remove Temp files before exiting
+      #
+      rm -rf $IESUBS/* IEDIR/*.txt
+fi
--- scripts/ie/ie-cleanup-filters.sh
+++ scripts/ie/ie-cleanup-filters.sh
@@ -1,73 +1,58 @@
 #!/bin/bash
 #
-# Fanboy IE Cleanup (31/07/2012)
+# Fanboy IE Cleanup (29/08/2012)
 # Dual License CCby3.0/GPLv2
 # http://creativecommons.org/licenses/by/3.0/
 # http://www.gnu.org/licenses/gpl-2.0.html
 #
-
-# Creating a 10Mb ramdisk Temp storage...
-#
-if [ ! -d "/tmp/ieramdisk/subscriptions" ]; then
-    rm -rf /tmp/ieramdisk/
-    mkdir /tmp/ieramdisk; chmod 777 /tmp/ieramdisk
-    mount -t tmpfs -o size=10M tmpfs /tmp/ieramdisk/
-    cp -f $MAINDIR/scripts/ie/combineSubscriptions.py /tmp/ieramdisk/
-    mkdir /tmp/ieramdisk/subscriptions
-    mkdir /tmp/ieramdisk/subscriptions/temp
-fi
 # Variables
 #
-export MAINDIR="/tmp/Ramdisk/www"
-export GOOGLEDIR="/tmp/hgstuff/fanboy-adblock-list"
-export TESTDIR="/tmp/work"
-export IEDIR="/tmp/ieramdisk"
-export SUBS="/tmp/ieramdisk/subscriptions"
-export ZIP="nice -n 19 /usr/local/bin/7za"
-export NICE="nice -n 19"
+
+export IEDIR="/tmp/work/ie"
+export IESUBS="/tmp/work/ie/subscriptions"
 
 # Now remove filters that cause issues in IE (and false positives)
 #
-sed -i '10,20000{/\#/d}' $SUBS/fanboy-noele.tpl $SUBS/fanboy-ultimate-ie.tpl $SUBS/fanboy-complete-ie.tpl
-sed -i '10,20000{/#/d}' $SUBS/fanboy-noele.tpl $SUBS/fanboy-ultimate-ie.tpl $SUBS/fanboy-complete-ie.tpl
-sed -i '/+d atdmt.com/d' $SUBS/fanboy-noele.tpl $SUBS/fanboy-ultimate-ie.tpl $SUBS/fanboy-complete-ie.tpl
-sed -i '/+d doubleclick.net/d' $SUBS/fanboy-noele.tpl $SUBS/fanboy-ultimate-ie.tpl $SUBS/fanboy-complete-ie.tpl
-sed -i '/+d 247realmedia.com/d' $SUBS/fanboy-noele.tpl $SUBS/fanboy-ultimate-ie.tpl $SUBS/fanboy-complete-ie.tpl
-sed -i '/+d googlesyndication.com/d' $SUBS/fanboy-noele.tpl $SUBS/fanboy-ultimate-ie.tpl $SUBS/fanboy-complete-ie.tpl
-sed -i '/+d scorecardresearch.com/d' $SUBS/fanboy-noele.tpl $SUBS/fanboy-ultimate-ie.tpl $SUBS/fanboy-complete-ie.tpl
-sed -i '/+d oascentral.thechronicleherald.ca/d' $SUBS/fanboy-noele.tpl $SUBS/fanboy-ultimate-ie.tpl $SUBS/fanboy-complete-ie.tpl
-sed -i '/+d au.adserver.yahoo.com/d' $SUBS/fanboy-noele.tpl $SUBS/fanboy-ultimate-ie.tpl $SUBS/fanboy-complete-ie.tpl
-sed -i '/+d adserver.yahoo.com/d' $SUBS/fanboy-noele.tpl $SUBS/fanboy-ultimate-ie.tpl $SUBS/fanboy-complete-ie.tpl
-sed -i '/+d skimlinks.com/d' $SUBS/fanboy-noele.tpl $SUBS/fanboy-ultimate-ie.tpl $SUBS/fanboy-complete-ie.tpl
-sed -i '/+d ad-emea.doubleclick.net/d' $SUBS/fanboy-noele.tpl $SUBS/fanboy-ultimate-ie.tpl $SUBS/fanboy-complete-ie.tpl
-sed -i '/+d ad.au.doubleclick.net/d' $SUBS/fanboy-noele.tpl $SUBS/fanboy-ultimate-ie.tpl $SUBS/fanboy-complete-ie.tpl
-sed -i '/+d spotxchange.com/d' $SUBS/fanboy-noele.tpl $SUBS/fanboy-ultimate-ie.tpl $SUBS/fanboy-complete-ie.tpl
-sed -i '/adf.ly/d' $SUBS/fanboy-noele.tpl $SUBS/fanboy-ultimate-ie.tpl $SUBS/fanboy-complete-ie.tpl
-sed -i '/+d ad-emea.doubleclick.net/d' $SUBS/fanboy-noele.tpl $SUBS/fanboy-ultimate-ie.tpl $SUBS/fanboy-complete-ie.tpl
-sed -i '/+d g.doubleclick.net/d' $SUBS/fanboy-noele.tpl $SUBS/fanboy-ultimate-ie.tpl $SUBS/fanboy-complete-ie.tpl
-sed -i '/+d pagead2.googlesyndication.com/d' $SUBS/fanboy-noele.tpl $SUBS/fanboy-ultimate-ie.tpl $SUBS/fanboy-complete-ie.tpl
-sed -i '/+d partner.googleadservices.com/d' $SUBS/fanboy-noele.tpl $SUBS/fanboy-ultimate-ie.tpl $SUBS/fanboy-complete-ie.tpl
-sed -i '/+d ads.yimg.com/d' $SUBS/fanboy-noele.tpl $SUBS/fanboy-ultimate-ie.tpl $SUBS/fanboy-complete-ie.tpl
-sed -i '/+d ad.ca.doubleclick.net/d' $SUBS/fanboy-noele.tpl $SUBS/fanboy-ultimate-ie.tpl $SUBS/fanboy-complete-ie.tpl
-sed -i '/+d ad.doubleclick.net/d' $SUBS/fanboy-noele.tpl $SUBS/fanboy-ultimate-ie.tpl $SUBS/fanboy-complete-ie.tpl
-sed -i '/+d zedo.com/d' $SUBS/fanboy-noele.tpl $SUBS/fanboy-ultimate-ie.tpl $SUBS/fanboy-complete-ie.tpl
+sed -i '10,20000{/\#/d}' $IESUBS/fanboy-noele.tpl $IESUBS/fanboy-ultimate-ie.tpl $IESUBS/fanboy-complete-ie.tpl
+sed -i '10,20000{/#/d}' $IESUBS/fanboy-noele.tpl $IESUBS/fanboy-ultimate-ie.tpl $IESUBS/fanboy-complete-ie.tpl
+sed -i '/+d atdmt.com/d' $IESUBS/fanboy-noele.tpl $IESUBS/fanboy-ultimate-ie.tpl $IESUBS/fanboy-complete-ie.tpl
+sed -i '/+d doubleclick.net/d' $IESUBS/fanboy-noele.tpl $IESUBS/fanboy-ultimate-ie.tpl $IESUBS/fanboy-complete-ie.tpl
+sed -i '/+d 247realmedia.com/d' $IESUBS/fanboy-noele.tpl $IESUBS/fanboy-ultimate-ie.tpl $IESUBS/fanboy-complete-ie.tpl
+sed -i '/+d googlesyndication.com/d' $IESUBS/fanboy-noele.tpl $IESUBS/fanboy-ultimate-ie.tpl $IESUBS/fanboy-complete-ie.tpl
+sed -i '/+d scorecardresearch.com/d' $IESUBS/fanboy-noele.tpl $IESUBS/fanboy-ultimate-ie.tpl $IESUBS/fanboy-complete-ie.tpl
+sed -i '/+d oascentral.thechronicleherald.ca/d' $IESUBS/fanboy-noele.tpl $IESUBS/fanboy-ultimate-ie.tpl $IESUBS/fanboy-complete-ie.tpl
+sed -i '/+d au.adserver.yahoo.com/d' $IESUBS/fanboy-noele.tpl $IESUBS/fanboy-ultimate-ie.tpl $IESUBS/fanboy-complete-ie.tpl
+sed -i '/+d adserver.yahoo.com/d' $IESUBS/fanboy-noele.tpl $IESUBS/fanboy-ultimate-ie.tpl $IESUBS/fanboy-complete-ie.tpl
+sed -i '/+d skimlinks.com/d' $IESUBS/fanboy-noele.tpl $IESUBS/fanboy-ultimate-ie.tpl $IESUBS/fanboy-complete-ie.tpl
+sed -i '/+d ad-emea.doubleclick.net/d' $IESUBS/fanboy-noele.tpl $IESUBS/fanboy-ultimate-ie.tpl $IESUBS/fanboy-complete-ie.tpl
+sed -i '/+d ad.au.doubleclick.net/d' $IESUBS/fanboy-noele.tpl $IESUBS/fanboy-ultimate-ie.tpl $IESUBS/fanboy-complete-ie.tpl
+sed -i '/+d spotxchange.com/d' $IESUBS/fanboy-noele.tpl $IESUBS/fanboy-ultimate-ie.tpl $IESUBS/fanboy-complete-ie.tpl
+sed -i '/adf.ly/d' $IESUBS/fanboy-noele.tpl $IESUBS/fanboy-ultimate-ie.tpl $IESUBS/fanboy-complete-ie.tpl
+sed -i '/+d ad-emea.doubleclick.net/d' $IESUBS/fanboy-noele.tpl $IESUBS/fanboy-ultimate-ie.tpl $IESUBS/fanboy-complete-ie.tpl
+sed -i '/+d g.doubleclick.net/d' $IESUBS/fanboy-noele.tpl $IESUBS/fanboy-ultimate-ie.tpl $IESUBS/fanboy-complete-ie.tpl
+sed -i '/+d pagead2.googlesyndication.com/d' $IESUBS/fanboy-noele.tpl $IESUBS/fanboy-ultimate-ie.tpl $IESUBS/fanboy-complete-ie.tpl
+sed -i '/+d partner.googleadservices.com/d' $IESUBS/fanboy-noele.tpl $IESUBS/fanboy-ultimate-ie.tpl $IESUBS/fanboy-complete-ie.tpl
+sed -i '/+d ads.yimg.com/d' $IESUBS/fanboy-noele.tpl $IESUBS/fanboy-ultimate-ie.tpl $IESUBS/fanboy-complete-ie.tpl
+sed -i '/+d ad.ca.doubleclick.net/d' $IESUBS/fanboy-noele.tpl $IESUBS/fanboy-ultimate-ie.tpl $IESUBS/fanboy-complete-ie.tpl
+sed -i '/+d ad.doubleclick.net/d' $IESUBS/fanboy-noele.tpl $IESUBS/fanboy-ultimate-ie.tpl $IESUBS/fanboy-complete-ie.tpl
+sed -i '/+d zedo.com/d' $IESUBS/fanboy-noele.tpl $IESUBS/fanboy-ultimate-ie.tpl $IESUBS/fanboy-complete-ie.tpl
 # http://hg.fanboy.co.nz/rev/5760d7c3afb3
-sed -i '/&adsType=/d' $SUBS/fanboy-noele.tpl $SUBS/fanboy-ultimate-ie.tpl $SUBS/fanboy-complete-ie.tpl
+sed -i '/&adsType=/d' $IESUBS/fanboy-noele.tpl $IESUBS/fanboy-ultimate-ie.tpl $IESUBS/fanboy-complete-ie.tpl
 
 # Now remove filters that cause issues in IE (and false positives)
 #
-sed -i '9,20000{/\#/d}' $SUBS/fanboy-tracking.tpl
-sed -i '9,20000{/#/d}' $SUBS/fanboy-tracking.tpl
+sed -i '9,20000{/\#/d}' $IESUBS/fanboy-tracking.tpl
+sed -i '9,20000{/#/d}' $IESUBS/fanboy-tracking.tpl
 
-sed -i '/Do-Not-Track/d' $SUBS/fanboy-tracking.tpl $SUBS/fanboy-ultimate-ie.tpl $SUBS/fanboy-complete-ie.tpl
-sed -i '/donottrack/d' $SUBS/fanboy-tracking.tpl $SUBS/fanboy-ultimate-ie.tpl $SUBS/fanboy-complete-ie.tpl
-sed -i '/-d nbcudigitaladops.com/d' $SUBS/fanboy-tracking.tpl $SUBS/fanboy-ultimate-ie.tpl $SUBS/fanboy-complete-ie.tpl
-sed -i '/-d dw.com.com/d' $SUBS/fanboy-tracking.tpl $SUBS/fanboy-ultimate-ie.tpl $SUBS/fanboy-complete-ie.tpl
-sed -i '/+d dhl./d' $SUBS/fanboy-tracking.tpl $SUBS/fanboy-ultimate-ie.tpl $SUBS/fanboy-complete-ie.tpl
-sed -i '/+d server-au.imrworldwide.com/d' $SUBS/fanboy-tracking.tpl $SUBS/fanboy-ultimate-ie.tpl $SUBS/fanboy-complete-ie.tpl
-sed -i '/+d secure-us.imrworldwide.com/d' $SUBS/fanboy-tracking.tpl $SUBS/fanboy-ultimate-ie.tpl $SUBS/fanboy-complete-ie.tpl
-sed -i '/+d revsci.net/d' $SUBS/fanboy-tracking.tpl $SUBS/fanboy-ultimate-ie.tpl $SUBS/fanboy-complete-ie.tpl
-sed -i '/+d js.revsci.net/d' $SUBS/fanboy-tracking.tpl $SUBS/fanboy-ultimate-ie.tpl $SUBS/fanboy-complete-ie.tpl
-sed -i '/+d easy.box/d' $SUBS/fanboy-tracking.tpl $SUBS/fanboy-ultimate-ie.tpl $SUBS/fanboy-complete-ie.tpl
-sed -i '/- \/quant.js/d' $SUBS/fanboy-tracking.tpl $SUBS/fanboy-ultimate-ie.tpl $SUBS/fanboy-complete-ie.tpl
-sed -i '/post-flair/d' $SUBS/fanboy-ultimate-ie.tpl $SUBS/fanboy-complete-ie.tpl
+sed -i '/Do-Not-Track/d' $IESUBS/fanboy-tracking.tpl $IESUBS/fanboy-ultimate-ie.tpl $IESUBS/fanboy-complete-ie.tpl
+sed -i '/donottrack/d' $IESUBS/fanboy-tracking.tpl $IESUBS/fanboy-ultimate-ie.tpl $IESUBS/fanboy-complete-ie.tpl
+sed -i '/-d nbcudigitaladops.com/d' $IESUBS/fanboy-tracking.tpl $IESUBS/fanboy-ultimate-ie.tpl $IESUBS/fanboy-complete-ie.tpl
+sed -i '/-d dw.com.com/d' $IESUBS/fanboy-tracking.tpl $IESUBS/fanboy-ultimate-ie.tpl $IESUBS/fanboy-complete-ie.tpl
+sed -i '/+d dhl./d' $IESUBS/fanboy-tracking.tpl $IESUBS/fanboy-ultimate-ie.tpl $IESUBS/fanboy-complete-ie.tpl
+sed -i '/+d server-au.imrworldwide.com/d' $IESUBS/fanboy-tracking.tpl $IESUBS/fanboy-ultimate-ie.tpl $IESUBS/fanboy-complete-ie.tpl
+sed -i '/+d secure-us.imrworldwide.com/d' $IESUBS/fanboy-tracking.tpl $IESUBS/fanboy-ultimate-ie.tpl $IESUBS/fanboy-complete-ie.tpl
+sed -i '/+d revsci.net/d' $IESUBS/fanboy-tracking.tpl $IESUBS/fanboy-ultimate-ie.tpl $IESUBS/fanboy-complete-ie.tpl
+sed -i '/+d js.revsci.net/d' $IESUBS/fanboy-tracking.tpl $IESUBS/fanboy-ultimate-ie.tpl $IESUBS/fanboy-complete-ie.tpl
+sed -i '/+d easy.box/d' $IESUBS/fanboy-tracking.tpl $IESUBS/fanboy-ultimate-ie.tpl $IESUBS/fanboy-complete-ie.tpl
+sed -i '/- \/quant.js/d' $IESUBS/fanboy-tracking.tpl $IESUBS/fanboy-ultimate-ie.tpl $IESUBS/fanboy-complete-ie.tpl
+sed -i '/post-flair/d' $IESUBS/fanboy-ultimate-ie.tpl $IESUBS/fanboy-complete-ie.tpl
