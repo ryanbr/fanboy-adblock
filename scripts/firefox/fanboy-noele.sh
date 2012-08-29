27cc54b717bf99c4389be5195874704b43ed7454
fanboy <devnull@localhost>
2012-08-29 19:04:29 +1200

    re-write fanboy-noele script generator

--- scripts/firefox/fanboy-noele.sh
+++ scripts/firefox/fanboy-noele.sh
@@ -1,44 +1,78 @@
 #!/bin/bash
 #
-# Fanboy Dimensions Adblock list grabber script v1.0 (26/05/2011)
+# Fanboy Non-element Adblock list grabber script v2.0 (26/08/2011)
 # Dual License CCby3.0/GPLv2
 # http://creativecommons.org/licenses/by/3.0/
 # http://www.gnu.org/licenses/gpl-2.0.html
 #
 
-# Creating a 20Mb ramdisk Temp storage...
+# Version history
 #
-if [ ! -d "/tmp/ramdisk/" ]; then
-    rm -rf /tmp/ramdisk/
-    mkdir /tmp/ramdisk; chmod 777 /tmp/ramdisk
-    mount -t tmpfs -o size=20M tmpfs /tmp/ramdisk/
-    mkdir /tmp/ramdisk/opera/
+# 2.00 Re-write script to be cleaner and more readable, better error checking.
+
+export ZIP="nice -n 19 /usr/local/bin/7za a -mx=9 -y -tgzip"
+export NICE="nice -n 19"
+export TAC="/usr/bin/tac"
+export CAT="/bin/cat"
+export MAINDIR="/tmp/Ramdisk/www/adblock"
+export SPLITDIR="/tmp/Ramdisk/www/adblock/split/test"
+export HGSERV="/tmp/hgstuff/fanboy-adblock-list"
+export TESTDIR="/tmp/work"
+export DATE="`date`"
+export ADDCHECKSUM="nice -n 19 perl $HGSERV/scripts/addChecksum.pl"
+export LOGFILE="/etc/crons/log.txt"
+export HG="/usr/local/bin/hg"
+export SHA256SUM="/usr/bin/sha256sum"
+export IEDIR="/tmp/ieramdisk"
+export TWIDGE="/usr/bin/twidge update"
+export SUBS="/tmp/ieramdisk/subscriptions"
+export IRONDIR="/tmp/Ramdisk/www/adblock/iron"
+
+# Cat the No-element list together
+#
+rm -rf $TESTDIR/fanboy-non-element.txt
+$CAT $HGSERV/fanboy-adblock/fanboy-header.txt \
+     $HGSERV/fanboy-adblock/fanboy-generic.txt \
+     $HGSERV/fanboy-adblock/fanboy-thirdparty.txt \
+     $HGSERV/fanboy-adblock/fanboy-firstparty.txt \
+     $HGSERV/fanboy-adblock/fanboy-popups.txt \
+     $HGSERV/fanboy-adblock/fanboy-whitelist.txt \
+     $HGSERV/fanboy-adblock/fanboy-dimensions.txt \
+     $HGSERV/fanboy-adblock/fanboy-dimensions-whitelist.txt \
+     $HGSERV/fanboy-adblock/fanboy-adult-generic.txt \
+     $HGSERV/fanboy-adblock/fanboy-adult-firstparty.txt \
+     $HGSERV/fanboy-adblock/fanboy-adult-thirdparty.txt \
+     $HGSERV/fanboy-adblock/fanboy-adult-whitelists.txt \
+     $HGSERV/fanboy-adblock/fanboy-p2p-firstparty.txt \
+     $HGSERV/fanboy-adblock/fanboy-p2p-thirdparty.txt > $TESTDIR/fanboy-non-element.txt
+
+# Make sure the file exists
+#
+if [ -s "$TESTDIR/fanboy-non-element.txt" ]; then
+
+          # Remove empty lines
+          #
+          sed '/^$/d' $TESTDIR/fanboy-non-element.txt > $TESTDIR/fanboy-non-element2.txt
+          mv -f $TESTDIR/fanboy-non-element2.txt $TESTDIR/fanboy-non-element.txt
+
+          # Title: Fanboy Adblock-Nonelements List
+          #
+          sed -i 's/Adblock\ List/Adblock-Nonelements\ List/g' $TESTDIR/fanboy-non-element.txt
+
+          # Re-do Checksum
+          #
+          $ADDCHECKSUM $TESTDIR/fanboy-non-element.txt
+
+          # Copy over files to webserver now
+          #
+          cp -f $TESTDIR/fanboy-non-element.txt $MAINDIR/fanboy-adblock-noele.txt
+          rm -rf $MAINDIR/fanboy-adblock-noele.txt.gz
+
+          # Gzip the file
+          #
+          $ZIP $MAINDIR/fanboy-adblock-noele.txt.gz $TESTDIR/fanboy-non-element.txt &> /dev/null
+    else
+          # If the Cat fails.
+          #
+          echo "Error creating file fanboy-non-element.txt: fanboy-nonele - $DATE" >> $LOGFILE
 fi
-
-# Variables for directorys
-#
-# MAINDIR="/var/www/adblock"
-# GOOGLEDIR="/home/fanboy/google/fanboy-adblock-list"
-# TESTDIR="/tmp/ramdisk"
-# ZIP="/usr/local/bin/7za"
-
-# Remove Temp files
-#
-rm -f $TESTDIR/fanboy-noele*.txt
-rm -f $TESTDIR/fanboy-adblocklist-current-expanded.txt
-
-cp -f $GOOGLEDIR/fanboy-adblocklist-current-expanded.txt $TESTDIR/fanboy-adblocklist-current-expanded.txt
-
-sed  -n '/Adblock Plus/,/p2p Element Firefox/{/p2p Element Firefox/!p}' $TESTDIR/fanboy-adblocklist-current-expanded.txt > $TESTDIR/fanboy-noele.txt
-sed -e '$d' $TESTDIR/fanboy-noele.txt > $TESTDIR/fanboy-noele2.txt
-perl $TESTDIR/addChecksum.pl $TESTDIR/fanboy-noele2.txt
-if diff $TESTDIR/fanboy-noele2.txt $MAINDIR/adblock/fanboy-adblock-noele.txt > /dev/null ; then
-    echo "No Changes detected: fanboy-adblock-noele.txt"
-  else
-    echo "Updated: fanboy-adblock-noele.txt"
-    rm -f $MAINDIR/adblock/fanboy-adblock-noele.txt.gz
-    cp -f $TESTDIR/adblock/fanboy-noele2.txt $MAINDIR/adblock/fanboy-adblock-noele.txt
-    $ZIP a -mx=9 -y -tgzip $MAINDIR/adblock/fanboy-adblock-noele.txt.gz $TESTDIR/fanboy-noele2.txt > /dev/null
-    # Generate IE script
-    $GOOGLEDIR/scripts/ie/adblock-ie-generator.sh
-fi 
