43df3255346187fe731fa522dfa4bfa1096348da
fanboy <devnull@localhost>
2014-10-25 16:41:08 +1300

    http://time.com/3536922/cable-ebola-coverage-story-versus-facts/

--- add-element.sh
+++ add-element.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 hg pull -u
 # Firefox Regional lists
-perl docs/Sorting/sorter2.pl fanboy-antifacebook.txt firefox-regional/fanboy-adblocklist-cz.txt firefox-regional/fanboy-adblocklist-esp.txt firefox-regional/fanboy-adblocklist-ind.txt firefox-regional/fanboy-adblocklist-ita.txt firefox-regional/fanboy-adblocklist-jpn.txt firefox-regional/fanboy-adblocklist-krn.txt firefox-regional/fanboy-adblocklist-pol.txt firefox-regional/fanboy-adblocklist-rus-v2.txt firefox-regional/fanboy-adblocklist-swe.txt firefox-regional/fanboy-adblocklist-tky.txt firefox-regional/fanboy-adblocklist-vtn.txt
+perl docs/Sorting/sorter2.pl fanboy-antifacebook.txt fanboy-antifonts.txt firefox-regional/fanboy-adblocklist-cz.txt firefox-regional/fanboy-adblocklist-esp.txt firefox-regional/fanboy-adblocklist-ind.txt firefox-regional/fanboy-adblocklist-ita.txt firefox-regional/fanboy-adblocklist-jpn.txt firefox-regional/fanboy-adblocklist-krn.txt firefox-regional/fanboy-adblocklist-pol.txt firefox-regional/fanboy-adblocklist-rus-v2.txt firefox-regional/fanboy-adblocklist-swe.txt firefox-regional/fanboy-adblocklist-tky.txt firefox-regional/fanboy-adblocklist-vtn.txt
 # Opera
 perl scripts/addChecksum-opera.pl opera/urlfilter.ini
 # Checksums for regionals and others
-perl scripts/addChecksum.pl fanboy-antifacebook.txt enhancedstats-addon.txt other/tracking-intl.txt firefox-regional/fanboy-adblocklist-cz.txt firefox-regional/fanboy-adblocklist-esp.txt firefox-regional/fanboy-adblocklist-ita.txt firefox-regional/fanboy-adblocklist-jpn.txt firefox-regional/fanboy-adblocklist-krn.txt firefox-regional/fanboy-adblocklist-swe.txt firefox-regional/fanboy-adblocklist-tky.txt firefox-regional/fanboy-adblocklist-vtn.txt firefox-regional/fanboy-adblocklist-ind.txt firefox-regional/fanboy-adblocklist-pol.txt
+perl scripts/addChecksum.pl fanboy-antifacebook.txt fanboy-antifonts.txt enhancedstats-addon.txt other/tracking-intl.txt firefox-regional/fanboy-adblocklist-cz.txt firefox-regional/fanboy-adblocklist-esp.txt firefox-regional/fanboy-adblocklist-ita.txt firefox-regional/fanboy-adblocklist-jpn.txt firefox-regional/fanboy-adblocklist-krn.txt firefox-regional/fanboy-adblocklist-swe.txt firefox-regional/fanboy-adblocklist-tky.txt firefox-regional/fanboy-adblocklist-vtn.txt firefox-regional/fanboy-adblocklist-ind.txt firefox-regional/fanboy-adblocklist-pol.txt
 # Commit
 hg commit -m "$1"
 hg push
