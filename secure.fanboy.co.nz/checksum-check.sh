c09969b6a0878b3bc1dfc0a101667b0bf06bd4ca
fanboynz <mp3geek@gmail.com>
2024-01-13 09:19:40 +0100

    Improve checksum checks

--- secure.fanboy.co.nz/checksum-check.sh
+++ secure.fanboy.co.nz/checksum-check.sh
@@ -23,32 +23,56 @@ else
     :
 fi
 
+# date
+export CURRENTDATE=$(date +"%Y%m%d_%H%M%S")
+
+# Diff logs
+export DIFFLOGS="/var/www/difflogs/diffs"
+# Ensure Logs exist.
+if [ ! -d "$DIFFLOGS" ]; then
+    mkdir -p "$DIFFLOGS"
+    echo "Folder created: $folder_path"
+else
+    # echo "Folder already exists: $folder_path"
+    :
+fi
+
 # VALIDCHECKSUM CHECKS
 export VALIDCHECKSUM="nice -n 19 perl /root/fanboy-adblock-list/scripts/validateChecksum.pl"
 
 cd $COOKDIR
 
-# extact gz to plain text.
+# easylist-cookie_ubo.txt.gz
 zcat $MAINDIR/easylist-cookie_ubo.txt.gz > $COOKDIR/easylist-cookie_ubo.txt.gz.zcat
-zcat $MAINDIR/easylist-cookie.txt.gz > $COOKDIR/easylist-cookie.txt.gz.zcat
-zcat $MAINDIR/fanboy-annoyance_ubo.txt.gz > $COOKDIR/fanboy-annoyance_ubo.txt.gz.zcat
-zcat $MAINDIR/fanboy-cookiemonster.txt.gz > $COOKDIR/fanboy-cookiemonster.txt.gz.zcat
-# agegate
-zcat $MAINDIR/fanboy-agegate.txt.gz > $COOKDIR/fanboy-agegate.txt.gz.zcat
-# notifications
-zcat $MAINDIR/fanboy-mobile-notifications.txt.gz > $COOKDIR/fanboy-mobile-notifications.txt.gz.zcat
-zcat $MAINDIR/fanboy-notifications.txt.gz > $COOKDIR/fanboy-notifications.txt.gz.zcat
-
-
 $VALIDCHECKSUM $COOKDIR/easylist-cookie_ubo.txt.gz.zcat > $COOKDIR/easylist-cookie_ubo.txt.gz.zcat.chk
+
+# easylist-cookie.txt.gz
+zcat $MAINDIR/easylist-cookie.txt.gz > $COOKDIR/easylist-cookie.txt.gz.zcat
 $VALIDCHECKSUM $COOKDIR/easylist-cookie.txt.gz.zcat > $COOKDIR/easylist-cookie.txt.gz.zcat.chk
+
+# fanboy-annoyance_ubo.txt.gz
+zcat $MAINDIR/fanboy-annoyance_ubo.txt.gz > $COOKDIR/fanboy-annoyance_ubo.txt.gz.zcat
 $VALIDCHECKSUM $COOKDIR/fanboy-annoyance_ubo.txt.gz.zcat > $COOKDIR/fanboy-annoyance_ubo.txt.gz.zcat.chk
+
+# fanboy-cookiemonster.txt.gz
+zcat $MAINDIR/fanboy-cookiemonster.txt.gz > $COOKDIR/fanboy-cookiemonster.txt.gz.zcat
 $VALIDCHECKSUM $COOKDIR/fanboy-cookiemonster.txt.gz.zcat > $COOKDIR/fanboy-cookiemonster.txt.gz.zcat.chk
-# agegate
+
+# fanboy-agegate.txt.gz
+zcat $MAINDIR/fanboy-agegate.txt.gz > $COOKDIR/fanboy-agegate.txt.gz.zcat
 $VALIDCHECKSUM $COOKDIR/fanboy-agegate.txt.gz.zcat > $COOKDIR/fanboy-agegate.txt.gz.zcat.chk
-# notifications
+
+# fanboy-mobile-notifications.txt.gz
+zcat $MAINDIR/fanboy-mobile-notifications.txt.gz > $COOKDIR/fanboy-mobile-notifications.txt.gz.zcat
 $VALIDCHECKSUM $COOKDIR/fanboy-mobile-notifications.txt.gz.zcat > $COOKDIR/fanboy-mobile-notifications.txt.gz.zcat.chk
+
+# fanboy-notifications.txt.gz
+zcat $MAINDIR/fanboy-notifications.txt.gz > $COOKDIR/fanboy-notifications.txt.gz.zcat
 $VALIDCHECKSUM $COOKDIR/fanboy-notifications.txt.gz.zcat > $COOKDIR/fanboy-notifications.txt.gz.zcat.chk
+
+# fanboy-mobile-notifications.txt (non-gzip)
+$VALIDCHECKSUM $MAINDIR/fanboy-mobile-notifications.txt > $COOKDIR/fanboy-mobile-notifications.txt.chk
+$VALIDCHECKSUM $MAINDIR/fanboy-notifications.txt > $COOKDIR/fanboy-notifications.txt.chk
 
 
 #### Non gzip'd (Easylist Cookie)
@@ -56,16 +80,45 @@ $VALIDCHECKSUM $COOKDIR/fanboy-notifications.txt.gz.zcat > $COOKDIR/fanboy-notif
 $VALIDCHECKSUM $MAINDIR/easylist-cookie.txt > $COOKDIR/easylist-cookie.txt.chk
 $VALIDCHECKSUM $MAINDIR/easylist-cookie_ubo.txt > $COOKDIR/easylist-cookie_ubo.txt.chk
 $VALIDCHECKSUM $MAINDIR/fanboy-annoyance.txt > $COOKDIR/fanboy-annoyance.txt.chk
-# Easylist Cookie (GZIP)
-files=("easylist-cookie_ubo.txt.gz.zcat.chk" "easylist-cookie.txt.gz.zcat.chk" "fanboy-annoyance_ubo.txt.gz.zcat.chk" "fanboy-cookiemonster.txt.gz.zcat.chk" "easylist-cookie.txt.chk" "easylist-cookie_ubo.txt.chk" "fanboy-annoyance.txt.chk")
+$VALIDCHECKSUM $COOKDIR/fanboy-agegate.txt > $COOKDIR/fanboy-agegate.txt.chk
+
+
+# Easylist Cookie + Fanboy Annoyances (GZIP)
+files=("easylist-cookie_ubo.txt.gz.zcat.chk" 
+       "easylist-cookie.txt.gz.zcat.chk" 
+       "fanboy-annoyance_ubo.txt.gz.zcat.chk" 
+       "fanboy-cookiemonster.txt.gz.zcat.chk" 
+       "easylist-cookie.txt.chk" 
+       "easylist-cookie_ubo.txt.chk" 
+       "fanboy-annoyance.txt.chk"
+)
 
 for file in "${files[@]}"; do
     if grep -q "\[Wrong checksum\]" "$file"; then
         echo "GZIP'd File '$file' contains '[Wrong checksum]'"
         . $CRONDIR/easylist-cookie-mirror.sh
-        
+        # Log checksums
+        cp -f easylist-cookie_ubo.txt.gz.zcat.chk $DIFFLOGS/easylist-cookie_ubo.txt.gz.zcat.$CURRENTDATE.chk
+        cp -f easylist-cookie.txt.gz.zcat.chk $DIFFLOGS/easylist-cookie.txt.gz.zcat.$CURRENTDATE.chk
+        cp -f fanboy-annoyance_ubo.txt.gz.zcat.chk $DIFFLOGS/fanboy-annoyance_ubo.txt.gz.zcat.$CURRENTDATE.chk
+        cp -f fanboy-cookiemonster.txt.gz.zcat.chk $DIFFLOGS/fanboy-cookiemonster.txt.gz.zcat.$CURRENTDATE.chk
+        cp -f easylist-cookie.txt.chk $DIFFLOGS/easylist-cookie.txt.$CURRENTDATE.chk
+        cp -f easylist-cookie_ubo.txt.chk $DIFFLOGS/easylist-cookie_ubo.txt.$CURRENTDATE.chk
+        cp -f fanboy-annoyance.txt.chk $DIFFLOGS/fanboy-annoyance.txt.$CURRENTDATE.chk
+        # Backup the gzip files
+        cp -f $MAINDIR/easylist-cookie_ubo.txt.gz $DIFFLOGS/easylist-cookie_ubo.$CURRENTDATE.txt.gz
+        cp -f $MAINDIR/easylist-cookie.txt.gz $DIFFLOGS/easylist-cookie.$CURRENTDATE.txt.gz
+        cp -f $MAINDIR/fanboy-annoyance_ubo.txt.gz $DIFFLOGS/fanboy-annoyance_ubo.$CURRENTDATE.txt.gz
+        cp -f $MAINDIR/fanboy-cookiemonster.txt.gz $DIFFLOGS/fanboy-cookiemonster.$CURRENTDATE.txt.gz
+        # Backup the plain txt files
+        cp -f $MAINDIR/easylist-cookie_ubo.txt $DIFFLOGS/easylist-cookie_ubo.$CURRENTDATE.txt
+        cp -f $MAINDIR/easylist-cookie.txt $DIFFLOGS/easylist-cookie.$CURRENTDATE.txt
+        cp -f $MAINDIR/fanboy-annoyance_ubo.txt $DIFFLOGS/fanboy-annoyance_ubo.$CURRENTDATE.txt
+        cp -f $MAINDIR/fanboy-cookiemonster.txt $DIFFLOGS/fanboy-cookiemonster.$CURRENTDATE.txt
+       
     else
-        echo "GZIP'd File '$file' does not contain '[Wrong checksum]'"
+        # echo "GZIP'd File '$file' does not contain '[Wrong checksum]'"
+        :
     fi
 done
 
@@ -78,14 +131,17 @@ for file in "${files[@]}"; do
     if grep -q "\[Wrong checksum\]" "$file"; then
         echo "GZIP'd File '$file' contains '[Wrong checksum]'"
         . $CRONDIR/age-gate.sh
-        
+        # Log checksums
+        cp -f fanboy-agegate.txt.chk $DIFFLOGS/fanboy-agegate.txt.$CURRENTDATE.chk
+        # Backup bad files
+        cp -f $MAINDIR/fanboy-agegate.txt.gz $MAINDIR/fanboy-agegate.$CURRENTDATE.txt.gz
+        cp -f $MAINDIR/fanboy-agegate.txt $MAINDIR/fanboy-agegate.$CURRENTDATE.txt
     else
-        echo "GZIP'd File '$file' does not contain '[Wrong checksum]'"
+        # echo "GZIP'd File '$file' does not contain '[Wrong checksum]'"
+        :
     fi
 done
 
-$VALIDCHECKSUM $MAINDIR/fanboy-mobile-notifications.txt > $COOKDIR/fanboy-mobile-notifications.txt.chk
-$VALIDCHECKSUM $MAINDIR/fanboy-notifications.txt > $COOKDIR/fanboy-notifications.txt.chk
 
 # Notifications List (GZIP)
 files=("fanboy-mobile-notifications.txt.gz.zcat.chk" "fanboy-notifications.txt.gz.zcat.chk" "fanboy-mobile-notifications.txt.chk" "fanboy-notifications.txt.chk")
@@ -94,9 +150,19 @@ for file in "${files[@]}"; do
     if grep -q "\[Wrong checksum\]" "$file"; then
         echo "GZIP'd File '$file' contains '[Wrong checksum]'"
         . $CRONDIR/make-notifications.sh
-        
+        # Log checksums
+        cp -f fanboy-mobile-notifications.txt.gz.zcat.chk $DIFFLOGS/fanboy-mobile-notifications.txt.gz.zcat.$CURRENTDATE.chk
+        cp -f fanboy-notifications.txt.gz.zcat.chk $DIFFLOGS/fanboy-notifications.txt.gz.zcat.$CURRENTDATE.chk
+        cp -f fanboy-mobile-notifications.txt.chk $DIFFLOGS/fanboy-mobile-notifications.txt.$CURRENTDATE.chk
+        cp -f fanboy-notifications.txt.chk $DIFFLOGS/fanboy-notifications.txt.$CURRENTDATE.chk
+        # Backup bad files
+        cp -f $MAINDIR/fanboy-mobile-notifications.txt.gz $DIFFLOGS/fanboy-mobile-notifications.$CURRENTDATE.txt.gz
+        cp -f $MAINDIR/fanboy-notifications.txt.gz $DIFFLOGS/fanboy-notifications.$CURRENTDATE.txt.gz
+        cp -f $MAINDIR/fanboy-mobile-notifications.txt $DIFFLOGS/fanboy-mobile-notifications.$CURRENTDATE.txt
+        cp -f $MAINDIR/fanboy-notifications.txt $DIFFLOGS/fanboy-notifications.$CURRENTDATE.txt
     else
-        echo "GZIP'd File '$file' does not contain '[Wrong checksum]'"
+        # echo "GZIP'd File '$file' does not contain '[Wrong checksum]'"
+        :
     fi
 done
 
