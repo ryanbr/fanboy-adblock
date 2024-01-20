824c7093790dcc84f29124b78f42f95855c8b23d
fanboynz <mp3geek@gmail.com>
2024-01-20 11:31:29 +0100

    Checksum fixes

--- secure.fanboy.co.nz/checksum-check.sh
+++ secure.fanboy.co.nz/checksum-check.sh
@@ -42,14 +42,6 @@ export VALIDCHECKSUM="nice -n 19 perl /root/fanboy-adblock/scripts/validateCheck
 
 cd $COOKDIR
 
-# easylist-cookie_ubo.txt.gz
-zcat $MAINDIR/easylist-cookie_ubo.txt.gz > $COOKDIR/easylist-cookie_ubo.txt.gz.zcat
-$VALIDCHECKSUM $COOKDIR/easylist-cookie_ubo.txt.gz.zcat > $COOKDIR/easylist-cookie_ubo.txt.gz.zcat.chk
-
-# easylist-cookie.txt.gz
-zcat $MAINDIR/easylist-cookie.txt.gz > $COOKDIR/easylist-cookie.txt.gz.zcat
-$VALIDCHECKSUM $COOKDIR/easylist-cookie.txt.gz.zcat > $COOKDIR/easylist-cookie.txt.gz.zcat.chk
-
 # fanboy-annoyance_ubo.txt.gz
 zcat $MAINDIR/fanboy-annoyance_ubo.txt.gz > $COOKDIR/fanboy-annoyance_ubo.txt.gz.zcat
 $VALIDCHECKSUM $COOKDIR/fanboy-annoyance_ubo.txt.gz.zcat > $COOKDIR/fanboy-annoyance_ubo.txt.gz.zcat.chk
@@ -57,6 +49,10 @@ $VALIDCHECKSUM $COOKDIR/fanboy-annoyance_ubo.txt.gz.zcat > $COOKDIR/fanboy-annoy
 # fanboy-cookiemonster.txt.gz
 zcat $MAINDIR/fanboy-cookiemonster.txt.gz > $COOKDIR/fanboy-cookiemonster.txt.gz.zcat
 $VALIDCHECKSUM $COOKDIR/fanboy-cookiemonster.txt.gz.zcat > $COOKDIR/fanboy-cookiemonster.txt.gz.zcat.chk
+
+# fanboy-cookiemonster_ubo.txt.gz
+zcat $MAINDIR/fanboy-cookiemonster_ubo.txt.gz > $COOKDIR/fanboy-cookiemonster_ubo.txt.gz.zcat
+$VALIDCHECKSUM $COOKDIR/fanboy-cookiemonster_ubo.txt.gz.zcat > $COOKDIR/fanboy-cookiemonster_ubo.txt.gz.zcat.chk
 
 # fanboy-agegate.txt.gz
 zcat $MAINDIR/fanboy-agegate.txt.gz > $COOKDIR/fanboy-agegate.txt.gz.zcat
@@ -77,45 +73,39 @@ $VALIDCHECKSUM $MAINDIR/fanboy-notifications.txt > $COOKDIR/fanboy-notifications
 
 #### Non gzip'd (Easylist Cookie)
 
-$VALIDCHECKSUM $MAINDIR/easylist-cookie.txt > $COOKDIR/easylist-cookie.txt.chk
-$VALIDCHECKSUM $MAINDIR/easylist-cookie_ubo.txt > $COOKDIR/easylist-cookie_ubo.txt.chk
+# $VALIDCHECKSUM $MAINDIR/easylist-cookie.txt > $COOKDIR/easylist-cookie.txt.chk
+# $VALIDCHECKSUM $MAINDIR/easylist-cookie_ubo.txt > $COOKDIR/easylist-cookie_ubo.txt.chk
+
+$VALIDCHECKSUM $MAINDIR/fanboy-cookiemonster.txt > $COOKDIR/fanboy-cookiemonster.txt.chk
+$VALIDCHECKSUM $MAINDIR/fanboy-cookiemonster_ubo.txt > $COOKDIR/fanboy-cookiemonster_ubo.txt.chk
 $VALIDCHECKSUM $MAINDIR/fanboy-annoyance.txt > $COOKDIR/fanboy-annoyance.txt.chk
-$VALIDCHECKSUM $COOKDIR/fanboy-agegate.txt > $COOKDIR/fanboy-agegate.txt.chk
 
 
 # Easylist Cookie + Fanboy Annoyances (GZIP)
-files=("easylist-cookie_ubo.txt.gz.zcat.chk" 
-       "easylist-cookie.txt.gz.zcat.chk" 
+files=("fanboy-annoyance.txt.chk"
        "fanboy-annoyance_ubo.txt.gz.zcat.chk" 
-       "fanboy-cookiemonster.txt.gz.zcat.chk" 
-       "easylist-cookie.txt.chk" 
-       "easylist-cookie_ubo.txt.chk" 
-       "fanboy-annoyance.txt.chk"
+       "fanboy-cookiemonster.txt.chk"       
+       "fanboy-cookiemonster_ubo.txt.chk"
+       "fanboy-cookiemonster.txt.gz.zcat.chk"
+       "fanboy-cookiemonster_ubo.txt.gz.zcat.chk"
 )
 
 for file in "${files[@]}"; do
     if grep -q "\[Wrong checksum\]" "$file"; then
         echo "GZIP'd File '$file' contains '[Wrong checksum]'"
-        . $CRONDIR/easylist-cookie-mirror.sh
+        echo "Logging Easylist Cookie changes"
         # Log checksums
-        cp -f easylist-cookie_ubo.txt.gz.zcat.chk $DIFFLOGS/easylist-cookie_ubo.txt.gz.zcat.$CURRENTDATE.chk
-        cp -f easylist-cookie.txt.gz.zcat.chk $DIFFLOGS/easylist-cookie.txt.gz.zcat.$CURRENTDATE.chk
         cp -f fanboy-annoyance_ubo.txt.gz.zcat.chk $DIFFLOGS/fanboy-annoyance_ubo.txt.gz.zcat.$CURRENTDATE.chk
         cp -f fanboy-cookiemonster.txt.gz.zcat.chk $DIFFLOGS/fanboy-cookiemonster.txt.gz.zcat.$CURRENTDATE.chk
-        cp -f easylist-cookie.txt.chk $DIFFLOGS/easylist-cookie.txt.$CURRENTDATE.chk
-        cp -f easylist-cookie_ubo.txt.chk $DIFFLOGS/easylist-cookie_ubo.txt.$CURRENTDATE.chk
         cp -f fanboy-annoyance.txt.chk $DIFFLOGS/fanboy-annoyance.txt.$CURRENTDATE.chk
         # Backup the gzip files
-        cp -f $MAINDIR/easylist-cookie_ubo.txt.gz $DIFFLOGS/easylist-cookie_ubo.$CURRENTDATE.txt.gz
-        cp -f $MAINDIR/easylist-cookie.txt.gz $DIFFLOGS/easylist-cookie.$CURRENTDATE.txt.gz
         cp -f $MAINDIR/fanboy-annoyance_ubo.txt.gz $DIFFLOGS/fanboy-annoyance_ubo.$CURRENTDATE.txt.gz
         cp -f $MAINDIR/fanboy-cookiemonster.txt.gz $DIFFLOGS/fanboy-cookiemonster.$CURRENTDATE.txt.gz
         # Backup the plain txt files
-        cp -f $MAINDIR/easylist-cookie_ubo.txt $DIFFLOGS/easylist-cookie_ubo.$CURRENTDATE.txt
-        cp -f $MAINDIR/easylist-cookie.txt $DIFFLOGS/easylist-cookie.$CURRENTDATE.txt
         cp -f $MAINDIR/fanboy-annoyance_ubo.txt $DIFFLOGS/fanboy-annoyance_ubo.$CURRENTDATE.txt
         cp -f $MAINDIR/fanboy-cookiemonster.txt $DIFFLOGS/fanboy-cookiemonster.$CURRENTDATE.txt
-       
+        . $CRONDIR/easylist-cookie-mirror-3.sh
+        echo "Updated Easylist Cookie"
     else
         # echo "GZIP'd File '$file' does not contain '[Wrong checksum]'"
         :
